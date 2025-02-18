6138f57d782c086cf84ee5e1b295ba135c127c80
callmeMARY <mgcababao@paterostechnologicalcollege.edu.ph>
2025-02-19 00:14:28 +0800

    myfirstcommit

---
+++ Prog1.java
@@ -0,0 +1,8 @@
+public class Prog1 {
+    public static void main(String[ ] args) {
+            System.out.printIn("PATEROS") ;
+            System.out.printIn("TECHNOLOGICAL") ;
+            System.out.printInt("COLLEGE") ;
+      }
+}
+ 
