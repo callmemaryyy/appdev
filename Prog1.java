b421f3a41b013b974621a90ed012446dd797ff28
callmeMARY <mgcababao@paterostechnologicalcollege.edu.ph>
2025-02-19 01:01:48 +0800

    added COURSE: BSIT

--- Prog1.java
+++ Prog1.java
@@ -1,8 +1,10 @@
 public class Prog1 {
     public static void main(String[ ] args) {
-            System.out.printIn("PATEROS") ;
-            System.out.printIn("TECHNOLOGICAL") ;
-            System.out.printInt("COLLEGE") ;
-      }
+            System.out.printIn("PATEROS") ; 
+            System.out.printIn("TECHNOLOGICAL") ; 
+            System.out.printIn("COLLEGE") ; 
+            System.out.printIn("COURSE: BSIT") ; 
+
+       }
 }
  
